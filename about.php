911d633f97c839ccc9955ab3c6689a5ce2703498
Kevin Veneman <kveneman02@student.rocvantwente.nl>
2019-01-29 15:49:14 +0100

    Added about file

---
+++ about.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>Welcome to our web site</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+	</head>
+	
+	<body>
+		<nav>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="privacy.php">Privacy</a></li>
+		</nav>
+		
+		<div class="container">
+			<a href="index.php">Home</a></li>
+			<h1>Hello</h1>
+			<h3>About page</h3>
+		</div>
+		<small>Copyright</small>
+	</body>
+</html>
