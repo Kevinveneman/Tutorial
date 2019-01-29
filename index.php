4ebb891e13c71c8dc919407feec878dc94f6d142
Kevin Veneman <kveneman02@student.rocvantwente.nl>
2019-01-29 15:48:46 +0100

    Added index file

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title>Welcome to our web site</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+	</head>
+	
+	<body>
+		<nav>
+			<li><a href="about.php">About</a></li>
+			<li><a href="privacy.php">Privacy</a></li>
+		</nav>
+		
+		<div class="container">
+			<a href="about.php">About</a></li>
+			<h1>Hello</h1>
+			<h3>Please enter your name:</h3>
+			<input type="text" name="uname" />
+		</div>
+		<small>Copyright</small>
+	</body>
+</html>
